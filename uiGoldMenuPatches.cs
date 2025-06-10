29eb6b475520d7768eee41d86d3a1b2bf1ca784a
ShadowRocks2 <44444243+ShadowRocks2@users.noreply.github.com>
2025-06-10 02:11:36 -0400

    Update uiGoldMenuPatches.cs

--- uiGoldMenuPatches.cs
+++ uiGoldMenuPatches.cs
@@ -1,33 +1,35 @@
-﻿using HarmonyLib;
-using System;
+using HarmonyLib;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using UnityEngine;
 
 namespace FTK_MultiMax_Rework {
     public class uiGoldMenuPatches {
         public static bool GoldAwake(uiGoldMenu __instance) {
             var m_GoldEntriesField = Traverse.Create(__instance).Field("m_GoldEntries");
+            var goldEntries = m_GoldEntriesField.GetValue<List<uiGoldMenuEntry>>();
 
             __instance.m_InputFocus = __instance.gameObject.GetComponent<FTKInputFocus>();
             __instance.m_InputFocus.m_InputMode = FTKInput.InputMode.InGameUI;
             __instance.m_InputFocus.m_Cancel = __instance.OnButtonCancel;
 
-            if (m_GoldEntriesField.GetValue() != null) {
-                int maxEntries = (GameFlowMC.gMaxPlayers - 2);
+            if (goldEntries != null) {
+                int maxEntries = Mathf.Max(0, GameFlowMC.gMaxPlayers - 2); // safe clamp
 
-                m_GoldEntriesField.GetValue<List<uiGoldMenuEntry>>().Add(__instance.m_FirstEntry);
+                goldEntries.Add(__instance.m_FirstEntry);
 
                 for (int i = 0; i < maxEntries; i++) {
                     uiGoldMenuEntry newEntry = UnityEngine.Object.Instantiate(__instance.m_FirstEntry);
                     newEntry.transform.SetParent(__instance.m_FirstEntry.transform.parent, worldPositionStays: false);
-                    m_GoldEntriesField.GetValue<List<uiGoldMenuEntry>>().Add(newEntry);
+                    goldEntries.Add(newEntry);
                 }
+
+                Debug.Log($"[MultiMax Rework] uiGoldMenu expanded to {goldEntries.Count} entries.");
+            }
+            else {
+                Debug.LogError("[MultiMax Rework] Failed to access m_GoldEntries.");
             }
 
-            return false;
+            return false; // skip original Awake()
         }
     }
 }
