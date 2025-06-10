cbf56c5f29ac60963f15d3e5b090fe61f575a03b
ShadowRocks2 <44444243+ShadowRocks2@users.noreply.github.com>
2025-06-10 02:02:35 -0400

    Update EncounterSessionPatches.cs

--- EncounterSessionPatches.cs
+++ EncounterSessionPatches.cs
@@ -1,24 +1,24 @@
-﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+using HarmonyLib;
 using UnityEngine;
-using static UnityEngine.UI.CanvasScaler;
 
 namespace FTK_MultiMax_Rework {
     public static class EncounterSessionPatches {
         public static void XPModifierPatch(ref FTKPlayerID _recvPlayer, ref int _xp, ref int _gold) {
+            var characterOverworldByFid = FTKHub.Instance.GetCharacterOverworldByFID(_recvPlayer);
 
-            CharacterOverworld characterOverworldByFid = FTKHub.Instance.GetCharacterOverworldByFID(_recvPlayer);
+            if (characterOverworldByFid == null) {
+                Debug.LogWarning($"[MultiMaxRework] No CharacterOverworld found for player {_recvPlayer}. XP/GOLD patch skipped.");
+                return;
+            }
 
-            float xpMod = characterOverworldByFid.m_CharacterStats.XpModifier;
-            float goldMod = characterOverworldByFid.m_CharacterStats.GoldModifier;
+            float xpMod = Mathf.Max(1f, characterOverworldByFid.m_CharacterStats.XpModifier);
+            float goldMod = Mathf.Max(1f, characterOverworldByFid.m_CharacterStats.GoldModifier);
 
             if (GameFlowMC.gMaxPlayers > 3) {
-                _xp = Mathf.RoundToInt((float)((_xp * xpMod) * 1.5));
-                _gold = Mathf.RoundToInt((float)((_gold * goldMod) * 1.5));
+                _xp = Mathf.RoundToInt((_xp * xpMod) * 1.5f);
+                _gold = Mathf.RoundToInt((_gold * goldMod) * 1.5f);
+
+                Debug.Log($"[MultiMaxRework] XP modified to {_xp}, GOLD modified to {_gold} for player {_recvPlayer}.");
             }
         }
     }
